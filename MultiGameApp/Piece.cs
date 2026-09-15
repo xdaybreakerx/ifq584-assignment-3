5590792233cfc24136852b31cb4834106632985c
Xander Salathe <xsalathe@gmail.com>
2026-09-15 16:16:43 +1000

    fix: remove pseudocode framework

--- MultiGameApp/Piece.cs
+++ MultiGameApp/Piece.cs
@@ -2,20 +2,15 @@ using System;
 
 namespace BoardGameFramework;
 
-// Piece abstract class
 public abstract class Piece
 {
-    // require every piece to provide a display value
     public abstract string DisplayValue();
 
-    // require every piece to state whether it can be reused
     public abstract bool IsReusable();
 }
 
-// NumberPiece inherits from Piece
 public class NumberPiece : Piece
 {
-    // store the number as an integer
     private readonly int _value;
 
     public NumberPiece(int value)
@@ -28,9 +23,6 @@ public class NumberPiece : Piece
         get { return _value; }
     }
 
-    // when display value is requested
-    // convert the number to text
-    // return the text
     public override string DisplayValue()
     {
         string displayedNumber = _value.ToString();
@@ -38,28 +30,20 @@ public class NumberPiece : Piece
         return displayedNumber;
     }
 
-    // when checking if the piece is reusable
-    // return false
     public override bool IsReusable()
     {
         return false;
     }
 }
 
-// MarkPiece inherits from Piece
 public class MarkPiece : Piece
 {
-    // store the mark as a string, such as "X" or "O"
     private readonly string _symbol;
 
     // the game assigns the symbol during setup/construction
     // this validation prevents an invalid game configuration
     public MarkPiece(string symbol)
     {
-        // when creating a MarkPiece
-        // check that the symbol is not empty
-        // store the symbol
-
         if (string.IsNullOrWhiteSpace(symbol))
         {
             throw new ArgumentException("The symbol cannot be empty", nameof(symbol));
@@ -73,16 +57,10 @@ public class MarkPiece : Piece
         get { return _symbol; }
     }
 
-    // when display value is requested
-    // return the symbol
-
     public override string DisplayValue()
     {
         return _symbol;
     }
-
-    // when checking if the piece is reusable
-    // return true
 
     public override bool IsReusable()
     {
