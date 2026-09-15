c653375bec646989433940935f68ea77612a80e1
Xander Salathe <xsalathe@gmail.com>
2026-09-15 16:17:46 +1000

    fix: namespace

--- MultiGameApp/Enums.cs
+++ MultiGameApp/Enums.cs
@@ -1,14 +1,16 @@
+namespace BoardGameFramework;
+
 public enum GameType
 {
     NUMERICAL_TIC_TAC_TOE,
     NOTAKTO,
-    GOMOKU
+    GOMOKU,
 }
 
 public enum GameMode
 {
     HUMAN_VS_HUMAN,
-    HUMAN_VS_COMPUTER
+    HUMAN_VS_COMPUTER,
 }
 
 public enum GameResult
@@ -16,7 +18,7 @@ public enum GameResult
     IN_PROGRESS,
     DRAW,
     PLAYER_ONE_WIN,
-    PLAYER_TWO_WIN
+    PLAYER_TWO_WIN,
 }
 
 public enum CommandType
@@ -25,5 +27,5 @@ public enum CommandType
     UNDO,
     REDO,
     SAVE,
-    HELP
+    HELP,
 }
