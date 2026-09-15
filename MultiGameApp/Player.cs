a83dcc87d6885a9ab90d109f9fc896bd3fa79d64
Xander Salathe <xsalathe@gmail.com>
2026-09-15 21:36:12 +1000

    feat: Player (abstract) wip

--- MultiGameApp/Player.cs
+++ MultiGameApp/Player.cs
@@ -16,18 +16,40 @@ public abstract class Player
     // store the supplied id
     // store the supplied name
     // store the supplied collection of pieces
+    protected Player(int id, string name, List<Piece> availablePieces)
+    {
+        this.id = id;
+        this.name = name;
+        this.availablePieces = availablePieces;
+    }
 
     // when the player id is requested
     // return the stored id
+    public int Id
+    {
+        get { return id; }
+    }
 
     // when the player name is requested
     // return the stored name
+    public string Name
+    {
+        get { return name; }
+    }
 
     // when the available pieces are requested
     // return the available pieces
+    public List<Piece> AvailablePieces
+    {
+        get { return availablePieces; }
+    }
 
     // when checking whether the player has a piece
     // return whether the available pieces contain the requested piece
+    public bool HasPiece(Piece piece)
+    {
+        return availablePieces.Contains(piece);
+    }
 
     // when using a piece
     // if the player does not have the requested piece
@@ -35,12 +57,34 @@ public abstract class Player
     // if the piece cannot be reused
     // remove it from the available pieces
     // return true
+    public bool UsePiece(Piece piece)
+    {
+        if (!HasPiece(piece))
+        {
+            return false;
+        }
+
+        if (!piece.IsReusable())
+        {
+            availablePieces.Remove(piece);
+        }
+
+        return true;
+    }
 
     // when restoring a piece
     // if the piece cannot be reused and is not already available
     // add it to the available pieces
+    public void RestorePiece(Piece piece)
+    {
+        if (!piece.IsReusable() && !HasPiece(piece))
+        {
+            availablePieces.Add(piece);
+        }
+    }
 
     // require every player to state whether it is computer controlled
+    public abstract bool IsComputer();
 }
 
 public class HumanPlayer : Player
