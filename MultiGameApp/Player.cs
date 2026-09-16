a8275000636a053069e5cd9c492d48bd75410ca0
Xander Salathe <xsalathe@gmail.com>
2026-09-16 17:33:04 +1000

    fix: remove pseudocode

--- MultiGameApp/Player.cs
+++ MultiGameApp/Player.cs
@@ -3,20 +3,13 @@ using System.Data.Common;
 
 namespace MultiGameApp;
 
-// Player abstract class
 public abstract class Player
 {
-    // store the player id as an integer
-    // store the player name as text
-    // store the pieces currently available to the player
     private int id;
     private string name;
     private List<Piece> availablePieces;
 
-    // when a Player is created with an id, name, and collection of pieces
-    // store the supplied id
-    // store the supplied name
-    // store the supplied collection of pieces
+
     protected Player(int id, string name, List<Piece> availablePieces)
     {
         this.id = id;
@@ -24,40 +17,28 @@ public abstract class Player
         this.availablePieces = availablePieces;
     }
 
-    // when the player id is requested
-    // return the stored id
+
     public int Id
     {
         get { return id; }
     }
 
-    // when the player name is requested
-    // return the stored name
     public string Name
     {
         get { return name; }
     }
 
-    // when the available pieces are requested
-    // return the available pieces
     public List<Piece> AvailablePieces
     {
         get { return availablePieces; }
     }
 
-    // when checking whether the player has a piece
-    // return whether the available pieces contain the requested piece
+
     public bool HasPiece(Piece piece)
     {
         return availablePieces.Contains(piece);
     }
 
-    // when using a piece
-    // if the player does not have the requested piece
-    // return false
-    // if the piece cannot be reused
-    // remove it from the available pieces
-    // return true
     public bool UsePiece(Piece piece)
     {
         if (!HasPiece(piece))
@@ -73,9 +54,6 @@ public abstract class Player
         return true;
     }
 
-    // when restoring a piece
-    // if the piece cannot be reused and is not already available
-    // add it to the available pieces
     public void RestorePiece(Piece piece)
     {
         if (!piece.IsReusable() && !HasPiece(piece))
@@ -84,19 +62,14 @@ public abstract class Player
         }
     }
 
-    // require every player to state whether it is computer controlled
     public abstract bool IsComputer();
 }
 
 public class HumanPlayer : Player
 {
-    // HumanPlayer inherits from Player
-    // inherit the player id, name, and available pieces
     public HumanPlayer(int id, string name, List<Piece> availablePieces)
         : base(id, name, availablePieces) { }
 
-    // when checking whether the human player is computer controlled
-    // return false
     public override bool IsComputer()
     {
         return false;
